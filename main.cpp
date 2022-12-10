1d485d7bfb264ad34c244adb6c0ff9a91375fd1b
FinnMx <finnmoorhouse@gmail.com>
2022-12-10 14:38:11 +0000

    FINAL

--- main.cpp
+++ main.cpp
@@ -8,6 +8,24 @@
 
 namespace fs = std::experimental::filesystem;
 
+//-----------------------------------------------------------------
+//-----------------------------------------------------------------
+//-- This program uses c++17 features in experimental, Therefore --
+//--  Your going to have to either compile in c++17 or you can   --
+//-- put the following in preprocessor definitions...            --
+//--                                                             --
+//-- _DEBUG;_CONSOLE;%(PreprocessorDefinitions)                  --
+//-- _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING;       --
+//-- _CRT_SECURE_NO_WARNINGS                                     --
+//--                                                             --
+//-- *these these should already be predefined                   --
+//--                                                             --
+//-- if you dont know how to do this do the following..          --
+//-- Project -> Adv Prog... properties -> C/C++ -> PreProcessor  --
+//-- -> Preprocessor Definitions...                              --
+//-----------------------------------------------------------------
+//-----------------------------------------------------------------
+
 int main() {
 
 	std::ios_base::sync_with_stdio(false);
